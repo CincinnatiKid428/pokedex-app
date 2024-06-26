ed87c5a393e00b3b0d4db24f1f6663464a9e0e4e
CincinnatiKid428 <pweaver.psu03@gmail.com>
2024-06-25 22:14:04 -0400

    Initial state for Pokedex application.  Updated js\scripts.js to contain array of pokemon objects, added commenting in css\styles.css and changed text on index.html page to an under construction message

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,45 @@
-alert("Hello world.");
+/* Pokemon list array containing pokemon objects, each containing 
+    information to display */
 
-let favoriteFood = "Chicken noodle soup"
-document.write(favoriteFood);
+let pokemonList = []; 
+
+// Adding 5 pokemon objects
+pokemonList[0] = {
+    name:'Charmander',
+    number: 4,
+    height: 0.6,
+    types: ['fire'],
+    nextEvolution: 'Charmeleon'
+};
+
+pokemonList[1] = {
+    name:'Aron',
+    number: 304,
+    height: 0.4, 
+    types: ['steel','rock'],
+    nextEvolution: 'Lairon'
+};
+
+pokemonList[2] = {
+    name:'Blitzle',
+    number: 522,
+    height: 0.8,
+    types: ['electric'],
+    nextEvolution: 'Zebstrika'
+};
+
+pokemonList[3] = {
+    name:'Minccino',
+    number: 572,
+    height: 0.4,
+    types: ['normal'],
+    nextEvolution: 'Cinccino'
+};
+
+pokemonList[4] = {
+    name:'Shroomish',
+    number: 285,
+    height: 0.4,
+    types: ['grass'],
+    nextEvolution: 'Breloom'
+};
